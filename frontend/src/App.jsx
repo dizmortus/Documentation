e241077331db76c2061bb884152d2686af01181f
dizmortus <mvysotski76@gmail.com>
2024-07-02 09:14:17 +0300

    закоментил

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainApp from './MainApp.jsx';
 import DynamicPage from './DynamicPage.jsx';
-import axios from 'axios';
 import TokenService from './services/TokenService';
 import { useDispatch, useSelector } from 'react-redux';
 import api from "./services/api";
@@ -11,48 +10,53 @@ function App() {
     const [isLoading, setIsLoading] = useState(true);
     const user = useSelector(state => state.user.user);
     const dispatch = useDispatch();
+/*
 
     useEffect(() => {
-        // Проверка аутентификации при загрузке приложения
-        const maxAttempts = 3;
-        let attempts = 0;
+        if (user) {
+            // Проверка аутентификации при загрузке приложения, если пользователь аутентифицирован
+            const maxAttempts = 3;
+            let attempts = 0;
 
-        const checkAuthentication = async () => {
-            try {
-                // Проверка аутентификации на сервере
-                const response = await api.get('/api/auth/check-auth', {
-                    headers: {
-                        'x-access-token': `${TokenService.getLocalAccessToken()}`
+            const checkAuthentication = async () => {
+                try {
+                    // Проверка аутентификации на сервере
+                    const response = await api.get('/api/auth/check-auth', {
+                        headers: {
+                            'x-access-token': `${TokenService.getLocalAccessToken()}`
+                        }
+                    });
+                    console.log('Authentication complete');
+                    console.log('Server response:', response.data);
+                    TokenService.setUser(response.data);
+                    // Пользователь аутентифицирован, не делаем ничего
+                } catch (error) {
+                    // Обработка ошибки аутентификации
+                    console.error('Authentication error:', error);
+                    attempts++;
+                    if (attempts < maxAttempts) {
+                        setTimeout(checkAuthentication, 1000); // Повторная попытка через 1 секунду
+                    } else {
+                        // Очистка данных пользователя и разлогин
+                        TokenService.removeUser();
+                        dispatch({ type: 'LOGOUT_USER' });
+                        console.error('Exceeded max authentication attempts.');
                     }
-                });
-                console.log('Authentication complete');
-                console.log('Server response:', response.data);
-                TokenService.setUser(response.data);
-                // Пользователь аутентифицирован, не делаем ничего
-            } catch (error) {
-                // Обработка ошибки аутентификации
-                console.error('Authentication error:', error);
-                attempts++;
-                if (attempts < maxAttempts) {
-                    setTimeout(checkAuthentication, 1000); // Повторная попытка через 1 секунду
-                } else {
-                    // Очистка данных пользователя и разлогин
-                    TokenService.removeUser();
-                    dispatch({ type: 'LOGOUT_USER' });
-                    console.error('Exceeded max authentication attempts.');
+                } finally {
+                    setIsLoading(false);
                 }
-            } finally {
-                console.log('User role:', user ? user.role : null);
-                setIsLoading(false);
-            }
-        };
+            };
 
-        checkAuthentication(); // Запуск первой попытки при монтировании компонента
-    }, [dispatch]);
+            checkAuthentication(); // Запуск первой попытки при монтировании компонента
+        } else {
+            setIsLoading(false);
+        }
+    }, [dispatch, user]);
 
     if (isLoading) {
         return <div>Authentication...</div>; // Показать загрузку, пока проверяется аутентификация
     }
+*/
 
     return (
         <BrowserRouter>
