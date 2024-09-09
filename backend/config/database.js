970f85271bf8c89b9ac80d061798c077c6e6cf6f
dizmortus <mvysotski76@gmail.com>
2024-09-09 12:04:45 +0300

    railway5

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,12 +5,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'postgres',
   port: process.env.DB_PORT,
-  dialectOptions: {
-    ssl: {
-      require: true, // Обязательно использовать SSL
-      rejectUnauthorized: true // Разрешить небезопасные сертификаты, если необходимо
-    }
-  }
 });
 
 const db = {};
