1d2691c166d74bf174ada4e25a52d73bcc56beb9
dizmortus <mvysotski76@gmail.com>
2024-09-09 04:04:56 +0300

    railway2

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,16 +5,25 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'postgres',
   port: process.env.DB_PORT,
+  dialectOptions: {
+    ssl: {
+      require: true, // Обязательно использовать SSL
+      rejectUnauthorized: false // Разрешить небезопасные сертификаты, если необходимо
+    }
+  }
 });
-const db={};
+
+const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+// Модели
 db.user = require("../models/User.js")(sequelize, Sequelize);
-
 db.refreshToken = require("../models/refreshToken.js")(sequelize, Sequelize);
-db.comment=require("../models/Comment.js")(sequelize, Sequelize);
-db.page=require("../models/Page.js")(sequelize, Sequelize);
+db.comment = require("../models/Comment.js")(sequelize, Sequelize);
+db.page = require("../models/Page.js")(sequelize, Sequelize);
+
+// Связи
 db.refreshToken.belongsTo(db.user, {
   foreignKey: 'userId', targetKey: 'id'
 });
@@ -22,13 +31,16 @@ db.user.hasOne(db.refreshToken, {
   foreignKey: 'userId', targetKey: 'id'
 });
 
+// Проверка связей, если есть
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+
+// Аутентификация базы данных
 sequelize.authenticate()
-  .then(() => console.log('Database connected...'))
-  .catch(err => console.log('Error: ' + err));
+    .then(() => console.log('Database connected...'))
+    .catch(err => console.log('Error: ' + err));
 
 module.exports = db;
