fc5944ee09dc37fcb872c38f17b77778f18a0f8f
Maxim Vysotsky <mvysotski76@gmail.com>
2024-12-16 16:47:02 +0300

    erfkmfemcфффффф

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ require('dotenv').config(); // Загрузка переменных окруж�
 const app = express(); // Экземпляр приложения
 
 const corsOptions = {
-  origin: ['https://documentation-2zzt.onrender.com'], // Укажите фронтенд URL https://documentation-5o5l.onrender.com
+  origin: ['https://documentation-5o5l.onrender.com'], // Укажите фронтенд URL https://documentation-5o5l.onrender.com
   credentials: true, // Для поддержки кук и сессий
 };
 
